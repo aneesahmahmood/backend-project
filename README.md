f10fc6f8055464eb617ba80d91b490c1dfb3d983
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-04-15 12:40:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Northcoders News API
+
+1. Create a `.env.development` and a `.env.test` file in the root directory of the project. Within the .env.development, create the following: 'PGDATABASE=nc_news' and within the test create: 'PGDATABASE=nc_news_test'.
---
+++ db/setup.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS nc_news_test;
+DROP DATABASE IF EXISTS nc_news;
+
+CREATE DATABASE nc_news_test;
+CREATE DATABASE nc_news;
+
+
+-- We'll have two databases in this project: one for real-looking dev data, and another for simpler test data.
+
+-- You will need to create two .env files for your project: .env.test and .env.development. Into each, add PGDATABASE=, with the correct database name for that environment (see /db/setup.sql for the database names). Double check that these .env files are .gitignored.
+
+-- You'll need to run npm install at this point.
