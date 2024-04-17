fefa50951e2fb5c3249176c0850682a235d15a22
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-04-17 10:47:58 +0100

    task7

--- models/article-models.js
+++ models/article-models.js
@@ -60,3 +60,15 @@ exports.checkArticleExists = (article_id) => {
       }
     });
 };
+
+exports.addComment = (newComment, article_id) => {
+  const { username, body } = newComment;
+  return db
+    .query(
+      `INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *;`,
+      [username, body, article_id]
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
