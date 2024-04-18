a221c049b3274ac08f17fd35e2724a386a3fd4c5
Aneesah Mahmood <aneesah.mahmood1@gmail.com>
2024-04-18 14:18:56 +0100

    updated connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,19 @@
-const { config } = require('dotenv');
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const poolConfig = {};
+const config = {};
 
-if (ENV === 'production') {
-  poolConfig.connectionString = process.env.DATABASE_URL;
-  poolConfig.max = 2;
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
-module.exports = new Pool(poolConfig);
+module.exports = new Pool(config);
